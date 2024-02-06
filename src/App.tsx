9fe271fd65da75f817f4a1894d7ce05827768397
Isabel Vieira <work.isabelvieira@gmail.com>
2024-02-05 21:11:09 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="m-6 h-full flex-1 rounded-2xl bg-white p-6">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
